132e8b8d0f5a77ee1b009e160677b26fbcdc4377
Olga Hauze <galaxy73rambler@yahoo.com>
2011-09-24 23:42:11 -0400

    fix

---
+++ config/deploy.rb
@@ -0,0 +1,50 @@
+require 'bundler/capistrano'
+
+
+
+#############################################################
+#	Application
+#############################################################
+set :application, "townofmassanutten.org"
+set :deploy_to, "/home/townofma/#{application}"
+
+#############################################################
+#	Settings
+#############################################################
+
+default_run_options[:pty] = true
+set :use_sudo, true
+ssh_options[:port] = 7822
+ssh_options[:keys] = %w(~/.ssh/id_dsa)
+set :scm_verbose, true
+
+#############################################################
+#	Servers
+#############################################################
+
+set :user, "townofma"
+set :domain, "townofma.www65.a2hosting.com"
+role :web, domain
+role :app, domain
+role :db, domain, :primary => true
+
+#############################################################
+#	Git
+#############################################################
+set :repository, "git://github.com/authentic/townofmassanutten.org.git"
+set :scm, "git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+#############################################################
+#	Passenger
+#############################################################
+
+ namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
+after :deploy, "passenger:restart"
